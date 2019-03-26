27aa006026fdee94d7e81d814c0c5aedc0743081
PiZZAD0X <bravoecholimalima@gmail.com>
2019-03-25 22:52:03 -0700

    Briefing Notes expanded

--- uo_sys_framework/BriefingModule/cfg3DEN/Briefing/MissionNotesMenu.hpp
+++ uo_sys_framework/BriefingModule/cfg3DEN/Briefing/MissionNotesMenu.hpp
@@ -1,6 +1,6 @@
 class EGVAR(Briefing,MissionNotes) {
     displayName = "Mission Notes";
-    collapsed = 1;
+    collapsed = 0;
     class Attributes {
         class EGVAR(Briefing,MissionNotes_Enable) {
             property = QEGVAR(Briefing,MissionNotes_Enable);
